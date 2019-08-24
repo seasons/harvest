b56799bd16817892bcc799de7c9f1440f6f3428d
Luc Succes <l2succes@gmail.com>
2019-08-23 22:22:45 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import styled from "styled-components"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    fontFamily: "ProximaNova-Semibold",
+    fontSize: 16,
+  },
+})
