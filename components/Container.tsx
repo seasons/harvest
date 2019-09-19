a1971cd2470a53242ca982f431ddafbf0df0b8c0
Luc Succes <l2succes@gmail.com>
2019-09-19 16:43:15 -0400

    Updates AppDelegate for RNN

---
+++ components/Container.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import styled from "styled-components/native"
+import { SafeAreaView } from "react-native"
+
+export const Container = ({ children }) => {
+  return (
+    <Outer>
+      <Content>
+        <SafeAreaView>{children}</SafeAreaView>
+      </Content>
+    </Outer>
+  )
+}
+
+const Outer = styled.View`
+  flex: 1;
+  background-color: black;
+`
+
+const Content = styled.View`
+  flex: 1;
+  background-color: white;
+  border-bottom-left-radius: 30;
+  border-bottom-right-radius: 30;
+  overflow: hidden;
+  margin-bottom: 10;
+`
--- screens/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigation } from 'react-native-navigation'
-
-
-export function registerScreens() {
-    Navigation.registerComponent('Home', () => require('./Home'))
-}
