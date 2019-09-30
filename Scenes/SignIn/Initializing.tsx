6f59efd9430ec9c3540227d9db7c7ee6d4d6d14b
Luc Succes <l2succes@gmail.com>
2019-09-30 08:49:54 -0400

    Integrate auth0 + AsyncStorage into the app

--- Scenes/SignIn/Initializing.tsx
+++ Scenes/SignIn/Initializing.tsx
@@ -1,11 +1,14 @@
 import React from "react"
-import { View } from "react-native"
+import { AsyncStorage, View } from "react-native"
 import { goToWelcome, goHome } from "../../Navigation"
 
 export class Initializing extends React.Component {
   async componentDidMount() {
     try {
-      if (false) {
+      let userSession = await AsyncStorage.getItem("userSession")
+      userSession = JSON.parse(userSession)
+
+      if (userSession && userSession.accessToken) {
         goHome()
       } else {
         goToWelcome()
--- Scenes/SignIn/SignIn.tsx
+++ Scenes/SignIn/SignIn.tsx
@@ -1,10 +1,23 @@
 import React from "react"
 import { Theme, Sans, TextInput, Box, Spacer, Button, Flex } from "../../components"
 import { Text } from "../../components/Typography"
-import { SafeAreaView, TouchableWithoutFeedback } from "react-native"
+import { Alert, AsyncStorage, SafeAreaView, TouchableWithoutFeedback } from "react-native"
 import { color } from "../../helpers"
+import Auth0 from "react-native-auth0"
+import { goHome } from "../../Navigation"
 
-export class SignIn extends React.Component {
+const credentials = {
+  domain: "seasons.auth0.com",
+  clientId: "ovWzqnt8Qv5lQ4dhzpxdFb2u4zjOg3Cm",
+}
+
+const auth0 = new Auth0(credentials)
+
+interface SignInProps {
+  onAuth: (credentials, profile) => void
+}
+
+export class SignIn extends React.Component<SignInProps> {
   state = {
     email: "",
     password: "",
@@ -22,7 +35,7 @@ export class SignIn extends React.Component {
   handleSignIn = () => {
     const { email, password, emailComplete } = this.state
     if (emailComplete && password.length) {
-      // Add mutation to sign in
+      this.login(email, password)
     }
   }
 
@@ -32,6 +45,65 @@ export class SignIn extends React.Component {
 
   handleApply = () => {
     //FIXME: Handle apply
+  }
+
+  onSuccess(credentials) {
+    auth0.auth
+      .userInfo({ token: credentials.accessToken })
+      .then(profile => {
+        this.props.onAuth(credentials, profile)
+      })
+      .catch(error => this.alert("Error", error.json.error_description))
+  }
+
+  login(username, password) {
+    auth0.auth
+      .passwordRealm({
+        username: username,
+        password: password,
+        realm: "Username-Password-Authentication",
+      })
+      .then(success => {
+        console.log(success)
+        AsyncStorage.setItem("userSession", JSON.stringify(success))
+        goHome()
+      })
+      .catch(error => {
+        this.alert("Error", error.json.description)
+      })
+  }
+
+  createUser(username, password) {
+    auth0.auth
+      .createUser({
+        email: username,
+        password: password,
+        connection: "Username-Password-Authentication",
+      })
+      .then(success => {
+        console.log(success)
+        this.alert("Success", "New user created")
+      })
+      .catch(error => {
+        this.alert("Error", error.json.description)
+      })
+  }
+
+  webAuth(connection) {
+    auth0.webAuth
+      .authorize({
+        scope: "openid profile email",
+        connection: connection,
+        audience: "https://" + credentials.domain + "/userinfo",
+      })
+      .then(credentials => {
+        this.onSuccess(credentials)
+      })
+      .catch(error => this.alert("Error", error.error_description))
+  }
+
+  alert(title, message) {
+    Alert.alert(title, message, [{ text: "OK", onPress: () => console.log("OK Pressed") }], { cancelable: false })
   }
 
   render() {
