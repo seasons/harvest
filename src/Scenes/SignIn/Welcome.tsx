1c10045b6a3418ce472640446f16113272f5a574
kierangillen <kgillen@gmail.com>
2019-11-12 17:23:06 -0500

    stash

--- src/Scenes/SignIn/Welcome.tsx
+++ src/Scenes/SignIn/Welcome.tsx
@@ -11,37 +11,35 @@ export const Welcome = ({ navigation }) => {
   const dimensions = Dimensions.get("window")
   return (
     <Container>
-      <>
-        <Wrapper>
-          <LottieView
-            source={require("../../../assets/animations/welcome.json")}
-            autoPlay
-            loop
-            style={{ height: dimensions.height, width: dimensions.width, alignSelf: "center", position: "absolute" }}
-          />
-          <Box p={2}>
-            <SeasonsLogoSVG width={40} height={40} />
-            <Spacer mb={4} />
-            <Sans color="white" size="3">
-              This is Seasons
-            </Sans>
-            <Sans size="3" color="gray">
-              A members only rental subscription service for menswear and streetwear.
-            </Sans>
-            <Spacer mb={4} />
-            <Button onPress={() => navigation.navigate("SignIn")} variant="primaryLight">
-              Sign in
-            </Button>
-            <Spacer mb={2} />
-            <Button
-              onPress={() => navigation.navigate("Webview", { uri: "http://signup.seasons.nyc/" })}
-              variant="secondaryLight"
-            >
-              Join the waitlist
-            </Button>
-          </Box>
-        </Wrapper>
-      </>
+      <Wrapper>
+        <LottieView
+          source={require("../../../assets/animations/welcome.json")}
+          autoPlay
+          loop
+          style={{ height: dimensions.height, width: dimensions.width, alignSelf: "center", position: "absolute" }}
+        />
+        <Box p={2}>
+          <SeasonsLogoSVG width={40} height={40} />
+          <Spacer mb={4} />
+          <Sans color="white" size="3">
+            This is Seasons
+          </Sans>
+          <Sans size="3" color="gray">
+            A members only rental subscription service for menswear and streetwear.
+          </Sans>
+          <Spacer mb={4} />
+          <Button onPress={() => navigation.navigate("SignIn")} variant="primaryLight">
+            Sign in
+          </Button>
+          <Spacer mb={2} />
+          <Button
+            onPress={() => navigation.navigate("Webview", { uri: "http://signup.seasons.nyc/" })}
+            variant="secondaryLight"
+          >
+            Join the waitlist
+          </Button>
+        </Box>
+      </Wrapper>
     </Container>
   )
 }
