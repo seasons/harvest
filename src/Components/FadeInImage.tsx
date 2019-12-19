9c850b4fc98ff4cf3405e210cb262271e36d9688
Luc Succes <l2succes@gmail.com>
2019-12-19 16:43:24 -0500

    Update image placeholder background color

--- src/Components/FadeInImage.tsx
+++ src/Components/FadeInImage.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { Image, ImageProps, View } from "react-native"
 import styled from "styled-components/native"
+
 import FadeIn from "@expo/react-native-fade-in-image"
 
 export const FadeInImage: React.FC<ImageProps> = props => {
   return (
     <Container>
-      <FadeIn>
+      <FadeIn placeholderStyle={{ backgroundColor: "#f6f6f6" }}>
         <Image {...props} />
       </FadeIn>
     </Container>
