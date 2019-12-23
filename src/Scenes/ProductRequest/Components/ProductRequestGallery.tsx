e40badbc7a4f6e76059037e29cd5270a52a4f7bd
Kevin Chan <kevinchan159@gmail.com>
2019-12-23 14:30:49 -0500

    Address PR comments

--- src/Scenes/ProductRequest/Components/ProductRequestGallery.tsx
+++ src/Scenes/ProductRequest/Components/ProductRequestGallery.tsx
@@ -14,13 +14,13 @@ export const ProductRequestGallery: React.FC<Props> = ({ images }) => {
   }
 
   return (
-    <Box style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', height: 256 }}>
+    <Container>
       <ImageContainer
         resizeMode="contain"
         style={{ flex: 3, marginRight: 8 }}
         source={{ uri: images[0] }}
       />
-      <Box style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+      <ColumnContainer>
         {images.slice(1, 4).map((image: String, index: number) =>
           <ImageContainer
             resizeMode="contain"
@@ -28,10 +28,23 @@ export const ProductRequestGallery: React.FC<Props> = ({ images }) => {
             source={{ uri: image }}
           />
         )}
-      </Box>
-    </Box>
+      </ColumnContainer>
+    </Container>
   )
 }
+
+const Container = styled(Box)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 256;
+`
+
+const ColumnContainer = styled(Box)`
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+`
 
 const ImageContainer = styled(Image)`
   border-width: 1px;
