f9408a5f0c870397f7f107ede106da629bb55570
Kevin Chan <kevinchan159@gmail.com>
2020-03-21 20:45:25 -0400

    Moves console

--- src/Scenes/Home/Home.tsx
+++ src/Scenes/Home/Home.tsx
@@ -167,6 +167,10 @@ export const Home = screenTrack()(({ navigation }) => {
     })
   }
 
+  if (feedbackData) {
+    console.log("FEEDBACK DATA:", feedbackData)
+  }
+
   return (
     <Container insetsBottom={false}>
       <Loader />
@@ -180,10 +184,6 @@ export const Home = screenTrack()(({ navigation }) => {
 
   if (error) {
     console.error("error /home/index.tsx: ", error)
-  }
-
-  if (feedbackData) {
-    console.log("FEEDBACK DATA:", feedbackData)
   }
 
   if (showLoader || !data) {
