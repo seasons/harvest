8fcb9b8f8a9d60531fc307223a688274c5b1d563
Kieran Gillen <kgillen@gmail.com>
2020-03-24 18:02:42 -0400

    Sorts variantSizes

--- src/Components/VariantSizes.tsx
+++ src/Components/VariantSizes.tsx
@@ -4,13 +4,35 @@ import { color } from "App/utils"
 import React from "react"
 import styled from "styled-components/native"
 
+const weightedSize = {
+  XXL: 6,
+  XL: 5,
+  L: 4,
+  M: 3,
+  S: 2,
+  XS: 1,
+  XXS: 0,
+}
+
+const sortedVariants = variants => {
+  const sortedVars = variants.sort((a, b) => {
+    if (weightedSize[a?.internalSize?.display] && weightedSize[b?.internalSize?.display]) {
+      return weightedSize[a?.internalSize?.display] - weightedSize[b?.internalSize?.display]
+    } else {
+      return a?.internalSize?.display - b.internalSize.display
+    }
+  })
+  return sortedVars
+}
+
 export const VariantSizes: React.FC<{
   variants: Homepage_homepage_sections_results_Product_variants[]
   size: "0" | "1"
 }> = ({ variants, size }) => {
+  const sortedSizes = sortedVariants(variants)
   return (
     <Flex flexDirection="row">
-      {variants.map((variant: any) => {
+      {sortedSizes.map((variant: any) => {
         const reservable = variant.reservable !== null && !!variant.reservable
         return (
           <Box key={variant.id} mr={0.5} style={{ position: "relative" }}>
