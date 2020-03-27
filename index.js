015891d8168ea86fe3efd7c04a6029b2ee9f6f48
Luc Succes <l2succes@gmail.com>
2020-03-27 13:34:10 -0400

    Comment out enableScreens

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { App } from "./src/App"
 import { AppRegistry } from "react-native"
 import * as Sentry from "@sentry/react-native"
 import "react-native-gesture-handler"
-import { enableScreens } from "react-native-screens"
+// import { enableScreens } from "react-native-screens"
 
 if (!__DEV__) {
   Sentry.init({
@@ -10,6 +10,9 @@ if (!__DEV__) {
   })
 }
 
-enableScreens()
+// This enables the native stack navigator using (react-native-screens)
+// https://reactnavigation.org/blog/2020/02/06/react-navigation-5.0/#native-stack-navigator
+// Leaving it commented out for now because there's glitch when navigating to the Product view
+// enableScreens()
 
 AppRegistry.registerComponent("seasons", () => App)
