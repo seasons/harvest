e14be6a07b9d3208d90b1d3234261cf1c7dd7d8f
Kevin Chan <kevinchan159@gmail.com>
2020-04-02 18:25:50 -0400

    Fixes me query

--- src/generated/GetMembershipInfo.ts
+++ src/generated/GetMembershipInfo.ts
@@ -9,21 +9,21 @@ import { Plan } from "./globalTypes";
 // GraphQL query operation: GetMembershipInfo
 // ====================================================
 
-export interface GetMembershipInfo_me_customer_user {
+export interface GetMembershipInfo_me_customer {
+  __typename: "Customer";
+  plan: Plan | null;
+}
+
+export interface GetMembershipInfo_me_user {
   __typename: "User";
   firstName: string;
   lastName: string;
 }
 
-export interface GetMembershipInfo_me_customer {
-  __typename: "Customer";
-  plan: Plan | null;
-  user: GetMembershipInfo_me_customer_user;
-}
-
 export interface GetMembershipInfo_me {
   __typename: "Me";
   customer: GetMembershipInfo_me_customer;
+  user: GetMembershipInfo_me_user;
 }
 
 export interface GetMembershipInfo {
