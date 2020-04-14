33474e40f44b0a288547df0b14a1f55713e48da6
Kieran Gillen <kgillen@gmail.com>
2020-04-14 18:10:03 -0400

    Fixes payment and shipping

--- src/Scenes/Account/PaymentAndShipping/PaymentAndShipping.tsx
+++ src/Scenes/Account/PaymentAndShipping/PaymentAndShipping.tsx
@@ -1,7 +1,7 @@
 import { Box, Container, FixedBackArrow, FixedButton, Flex, Sans, Spacer } from "App/Components"
 import { Loader } from "App/Components/Loader"
 import gql from "graphql-tag"
-import React from "react"
+import React, { useEffect } from "react"
 import { useQuery } from "react-apollo"
 import { FlatList } from "react-native"
 import { AccountSection } from "../PersonalPreferences/PersonalPreferences"
@@ -52,7 +52,7 @@ export const GET_PAYMENT_DATA = gql`
   }
 `
 
-export const createShippingAddress = shippingAddress => {
+export const createShippingAddress = (shippingAddress) => {
   const addressArray = []
   if (shippingAddress.address1) {
     addressArray.push(shippingAddress.address1)
@@ -66,7 +66,7 @@ export const createShippingAddress = shippingAddress => {
   return addressArray
 }
 
-export const createBillingAddress = billingInfo => {
+export const createBillingAddress = (billingInfo) => {
   const addressArray = []
   if (billingInfo.street1) {
     addressArray.push(billingInfo.street1)
@@ -81,12 +81,48 @@ export const createBillingAddress = billingInfo => {
 }
 
 export const PaymentAndShipping = screenTrack()(({ navigation }) => {
-  const { loading, error, data } = useQuery(GET_PAYMENT_DATA, {
-    // Refetch query every second to account for chargebee webhook updates
-    pollInterval: 1000,
-  })
+  let timeout1
+  let timeout2
+  let timeout3
+  let timeout4
+  const { error, data, refetch } = useQuery(GET_PAYMENT_DATA)
+  useEffect(() => {
+    // The Chargebee address update takes multiple seconds to update
+    // therefore we must check and refetch data if the user leaves this view
+    const unsubscribe = navigation.addListener("focus", () => {
+      if (data) {
+        clearTimeout(timeout1)
+        clearTimeout(timeout2)
+        clearTimeout(timeout3)
+        clearTimeout(timeout4)
+        timeout1 = setTimeout(() => {
+          refetch()
+        }, 1000)
+        timeout2 = setTimeout(() => {
+          refetch()
+        }, 5000)
+        timeout3 = setTimeout(() => {
+          refetch()
+        }, 10000)
+        timeout4 = setTimeout(() => {
+          refetch()
+        }, 20000)
+      }
+    })
 
-  if (!data && loading) {
+    return unsubscribe
+  }, [navigation])
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeout1)
+      clearTimeout(timeout2)
+      clearTimeout(timeout3)
+      clearTimeout(timeout4)
+    }
+  }, [])
+
+  if (!data) {
     return <Loader />
   }
 
@@ -134,7 +170,7 @@ export const PaymentAndShipping = screenTrack()(({ navigation }) => {
     })
   }
 
-  const renderItem = item => {
+  const renderItem = (item) => {
     return <AccountSection title={item.title} value={item.value} />
   }
 
@@ -150,7 +186,7 @@ export const PaymentAndShipping = screenTrack()(({ navigation }) => {
             <Spacer mb={4} />
           </Box>
         )}
-        keyExtractor={item => item.title}
+        keyExtractor={(item) => item.title}
         renderItem={({ item }) => renderItem(item)}
       />
       <FixedButton block variant="primaryWhite" onPress={handleEditBtnPressed}>
