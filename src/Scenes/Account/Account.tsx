1f2ab6deca245ead8c817417a4abb635c3aa8a88
Kieran Gillen <kgillen@gmail.com>
2020-04-16 10:32:30 -0400

    Redefines hex color for themed color

--- src/Scenes/Account/Account.tsx
+++ src/Scenes/Account/Account.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, GuestView, Sans, Separator, Spacer } from "App/Components"
 import { useAuthContext } from "App/Navigation/AuthContext"
-import { space } from "App/utils"
+import { space, color } from "App/utils"
 import { Container } from "Components/Container"
 import gql from "graphql-tag"
 import React, { useEffect } from "react"
@@ -10,7 +10,6 @@ import { Linking, ScrollView, TouchableOpacity } from "react-native"
 import * as Animatable from "react-native-animatable"
 import { animated, useSpring } from "react-spring/native.cjs"
 import styled from "styled-components/native"
-import { color } from "styled-system"
 import { NotificationToggle } from "./Components/NotificationToggle"
 import { ProfileList } from "./ProfileList"
 import { screenTrack, useTracking, Schema } from "App/utils/track"
@@ -202,7 +201,7 @@ const LoaderContainer = animated(styled(Box)`
 
 const UserProfileLoader = () => {
   return (
-    <ContentLoader height={100} primaryColor="#f6f6f6">
+    <ContentLoader height={100} primaryColor={color("black04")}>
       <Rect x="0" y="5" width="120" height="20" />
       <Rect x="0" y="35" width="80" height="20" />
       <Rect x="90" y="35" width="90" height="20" />
