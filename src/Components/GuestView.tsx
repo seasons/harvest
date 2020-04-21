a4b9d246326f2228e37bea0e72f82a1cdfb18a9c
Kieran Gillen <kgillen@gmail.com>
2020-04-21 16:21:24 -0400

    Updates guest view

--- src/Components/GuestView.tsx
+++ src/Components/GuestView.tsx
@@ -1,57 +1,65 @@
 import React from "react"
-import { Flex, Button, Container, Sans, Box, Spacer } from "App/Components"
+import { Flex, Container, Sans, Box, Spacer } from "App/Components"
 import { SeasonsLogoSVG } from "Assets/svgs"
-import { color, space } from "App/utils"
-import { TouchableOpacity, Linking, Dimensions } from "react-native"
-
-const buttonWidth = (Dimensions.get("window").width - space(4)) / 2 - 1
+import { color } from "App/utils"
+import { TouchableOpacity, Linking, Text } from "react-native"
+import { Separator } from "./Separator"
+import { ButtonWithArrow } from "./ButtonWithArrow"
 
 export const GuestView = ({ navigation }) => {
   return (
     <Container>
-      <Flex style={{ flex: 1 }} alignContent="center" flexDirection="column" justifyContent="flex-end">
+      <Flex style={{ flex: 1 }} flexDirection="column" justifyContent="space-between">
         <Box px={2}>
+          <Spacer mb={64} />
           <SeasonsLogoSVG width={40} height={40} />
-          <Spacer mb={40} />
+          <Spacer mb={64} />
           <Sans color={color("black100")} size="3">
-            Welcome to Seasons.
+            Join the community
           </Sans>
+          <Spacer mb={0.5} />
           <Box pr={3}>
-            <Sans size="3" color={color("black50")}>
+            <Sans size="2" color={color("black50")}>
               Sign in to start reserving or join the waitlist to secure your spot and get an invite.
             </Sans>
           </Box>
-          <Spacer mb={40} />
-          <Button variant="primaryBlack" onPress={() => navigation.navigate("Modal", { screen: "SignInModal" })} block>
-            Sign in
-          </Button>
-          <Spacer mb={1} />
-          <Button
-            variant="primaryWhite"
-            onPress={() => navigation.navigate("Webview", { uri: "http://signup.seasons.nyc/" })}
-            block
-          >
+        </Box>
+        <Box px={2}></Box>
+        <Box px={2}>
+          <ButtonWithArrow onPress={() => navigation.navigate("Webview", { uri: "http://signup.seasons.nyc/" })}>
             Join the waitlist
-          </Button>
-          <Spacer mb={40} />
-          <Flex flexDirection="row" flexWrap="nowrap" justifyContent="space-between" alignItem="center">
+          </ButtonWithArrow>
+          <Spacer mb={3} />
+          <Flex flexDirection="row">
             <TouchableOpacity onPress={() => Linking.openURL(`mailto:membership@seasons.nyc`)}>
-              <Flex style={{ width: buttonWidth }} flexDirection="row" justifyContent="center">
-                <Sans size="2" color={color("black50")}>
-                  Contact
-                </Sans>
-              </Flex>
+              <Sans size="2" color={color("black50")} style={{ textDecorationLine: "underline" }}>
+                Contact
+              </Sans>
             </TouchableOpacity>
-            <Box style={{ height: 28, width: 1, backgroundColor: color("black50") }} />
+            <Spacer mr={3} />
             <TouchableOpacity onPress={() => navigation.navigate("Webview", { uri: "https://www.seasons.nyc" })}>
-              <Flex style={{ width: buttonWidth }} flexDirection="row" justifyContent="center">
-                <Sans size="2" color={color("black50")}>
-                  Learn More
-                </Sans>
-              </Flex>
+              <Sans size="2" color={color("black50")} style={{ textDecorationLine: "underline" }}>
+                Learn more
+              </Sans>
             </TouchableOpacity>
           </Flex>
-          <Spacer mb={40} />
+          <Spacer mb={5} />
+          <Separator />
+          <Spacer mb={2} />
+          <Text>
+            <Sans size="2" color={color("black50")}>
+              Already a member?
+            </Sans>{" "}
+            <Sans
+              size="2"
+              color={color("black50")}
+              style={{ textDecorationLine: "underline" }}
+              onPress={() => navigation.navigate("Modal", { screen: "SignInModal" })}
+            >
+              Login
+            </Sans>
+            <Spacer mb={2} />
+          </Text>
         </Box>
       </Flex>
     </Container>
