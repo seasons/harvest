a25f4c7c8bff23260c909274f79347609305fb2e
Kieran Gillen <kgillen@gmail.com>
2020-06-11 16:01:19 -0400

    Fixes history from crashing app

--- src/Scenes/Bag/Components/ReservationHistoryTab.tsx
+++ src/Scenes/Bag/Components/ReservationHistoryTab.tsx
@@ -6,10 +6,11 @@ import { BagView } from "../Bag"
 import { color } from "App/utils"
 
 export const ReservationHistoryTab: React.FC<{ items }> = ({ items }) => {
+  console.log("items", items)
   return (
     <Flex style={{ flex: 1 }}>
-      {items.length ? (
-        items.map((bagItem, index) => {
+      {items?.length ? (
+        items?.map((bagItem, index) => {
           return (
             <Box key={index}>
               <ReservationHistoryItem item={bagItem} />
--- src/Scenes/Reservation/Reservation.tsx
+++ src/Scenes/Reservation/Reservation.tsx
@@ -2,7 +2,7 @@ import { Box, Container, FixedBackArrow, FixedButton, Flex, Sans, Separator, Spa
 import { Loader } from "App/Components/Loader"
 import { usePopUpContext } from "App/Navigation/PopUp/PopUpContext"
 import { GET_BAG } from "App/Scenes/Bag/BagQueries"
-import { color, space } from "App/utils"
+import { space } from "App/utils"
 import { Schema, screenTrack, useTracking } from "App/utils/track"
 import gql from "graphql-tag"
 import React, { useState } from "react"
