387a111820b961d2baa2e2f8dcdc483cab910409
Kieran Gillen <kgillen@gmail.com>
2020-06-11 12:18:31 -0400

    Updates FAQ

--- src/Scenes/Faq/__tests__/Faq-tests.tsx
+++ src/Scenes/Faq/__tests__/Faq-tests.tsx
@@ -12,22 +12,22 @@ const FaqFixture = {
         title: "Need to return your items?",
         subsections: [
           {
-            title: "Ready for something new?",
+            title: "Ready to return your items?",
             text:
-              "We're happy to exchange for a different size pending availability. Pack it up, attach the pre-paid shipping label and drop it off at your closest UPS or UPS pickup point.",
+              "Pack up the pieces you want to return with the included hangers, attach the pre-paid shipping label and drop it off at your closest UPS or UPS pickup location.",
+          },
+          {
+            title: "Did something not fit?",
+            text: "We’re happy to help you find something that fits. Pack it up, send it back, and we’ll swap it out.",
           },
           {
             title: "Received the wrong item?",
             text:
-              "Sorry about that! Pack it up, attach the pre-paid shipping label and send it back. We'll get you the right one.",
+              "Sorry about that! Pack it up, attach the pre-paid shipping label and send it back. We’ll get you the right one.",
           },
           {
-            title: "Did something not fit?",
-            text: "We're happy to help you find something that fits. Pack it up, send it back, and we'll swap it out.",
-          },
-          {
-            title: "Have feedback or thoughts?",
-            text: "Contact us below. We'd love to hear from you.",
+            title: "Lost your return label?",
+            text: "It happens. We understand. Contact us below and we’ll send you a new label.",
           },
         ],
       },
