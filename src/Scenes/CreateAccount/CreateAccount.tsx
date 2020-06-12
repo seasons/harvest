067ddc8d522808af64b7a0b282dd2a66c43e3c05
danielVebman <danveb800@gmail.com>
2020-06-12 13:42:22 -0400

    Implement keyboard awareness and autoscroll

--- src/Scenes/CreateAccount/CreateAccount.tsx
+++ src/Scenes/CreateAccount/CreateAccount.tsx
@@ -1,14 +1,22 @@
 import { Box, Button, CloseButton, Container, Flex, Sans, Spacer, TextInput } from "App/Components"
 import { color } from "App/utils"
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState, MutableRefObject } from "react"
 import { Text } from "Components/Typography"
-import { TouchableWithoutFeedback } from "react-native"
+import { Keyboard, KeyboardEvent, ScrollView, TouchableWithoutFeedback } from "react-native"
 import { isValidEmail } from "App/helpers/regex"
-import { ScrollView } from "react-native"
+import { BoxProps } from "App/Components/Box"
 
 interface CreateAccountProps {
     onAuth: (credentials, profile) => void
     navigation: any
+}
+
+interface FrameAwareBoxProps extends BoxProps {
+    didGetFrame: (number) => void
+}
+
+const FrameAwareBox: React.FC<FrameAwareBoxProps> = (props) => {
+    return <Box {...props} />
 }
 
 export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
@@ -18,7 +26,41 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
     const [dateOfBirth, setDateOfBirth] = useState("")
     const [zipCode, setZipCode] = useState("")
 
-    const [extraScrollHeight, setExtraScrollheight] = useState(0)
+    const [keyboardHeight, setKeyboardHeight] = useState(0)
+    const [scrollViewHeight, setScrollViewHeight] = useState(0)
+    const [keyboardBoxMinY, setKeyboardBoxMinY] = useState(0)
+    const [footerBoxHeight, setFooterBoxHeight] = useState(0)
+    const scrollViewRef: MutableRefObject<ScrollView> = useRef();
+
+    useEffect(() => {
+        Keyboard.addListener("keyboardDidShow", onKeyboardDidShow)
+        Keyboard.addListener("keyboardDidHide", onKeyboardDidHide);
+
+        return () => {
+            Keyboard.removeListener("keyboardDidShow", onKeyboardDidShow)
+            Keyboard.removeListener("keyboardDidHide", onKeyboardDidHide)
+        }
+    }, [])
+
+    const onKeyboardDidShow = (e: KeyboardEvent) => {
+        setKeyboardHeight(e.endCoordinates.height)
+    }
+
+    const onKeyboardDidHide = () => {
+        if (scrollViewRef) {
+            scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: true })
+            setTimeout(() => {
+                setKeyboardHeight(0)
+            }, 200);
+        } else {
+            setKeyboardHeight(0)
+        }
+    }
+
+    const onFocusTextInput = (index: number) => {
+        if (!scrollViewRef.current) { return }
+        scrollViewRef.current.scrollTo({ x: 0, y: index * 56, animated: true })
+    }
 
     const onDateOfBirthChange = (val) => {
         if (val.length === 2 || val.length === 5) {
@@ -30,14 +72,18 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
         }
     }
 
+    const onZipCodeChange = (val) => {
+        val.length <= 5 ? setZipCode(val) : setZipCode(val.slice(0, 5))
+    }
+
     return (
         <Container insetsBottom={false} insetsTop={false}>
             <CloseButton />
             <Flex style={{ flex: 1 }}>
-                <Spacer mb={3} />
-                <Flex flexDirection="column" style={{ flex: 1 }}>
-                    <Box p={2} mt={5} style={{ backgroundColor: "red" }}>
-                        <ScrollView showsVerticalScrollIndicator={false} keyboardDismissMode="interactive">
+                <Flex flexDirection="column" style={{ flex: 1, justifyContent: "space-between" }}>
+                    <ScrollView showsVerticalScrollIndicator={false} keyboardDismissMode="interactive" onLayout={e => setScrollViewHeight(e.nativeEvent.layout.height)} ref={scrollViewRef}>
+                        <Spacer mb={5} />
+                        <Box p={4} pb={5}>
                             <Sans color={color("black100")} size="3">
                                 Create an account
                             </Sans>
@@ -52,6 +98,7 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                 inputKey="full-name"
                                 autoCapitalize="words"
                                 onChangeText={(_, val) => setName(val)}
+                                onFocus={() => onFocusTextInput(0)}
                             />
                             <Spacer mb={2} />
                             <TextInput
@@ -60,6 +107,7 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                 inputKey="email"
                                 keyboardType="email-address"
                                 onChangeText={(_, val) => setEmail(val)}
+                                onFocus={() => onFocusTextInput(1)}
                             />
                             <Spacer mb={2} />
                             <TextInput
@@ -68,6 +116,7 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                 variant="light"
                                 inputKey="password"
                                 onChangeText={(_, val) => setPassword(val)}
+                                onFocus={() => onFocusTextInput(2)}
                             />
                             <Spacer mb={2} />
                             <TextInput
@@ -77,6 +126,7 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                 inputKey="date-of-birth"
                                 keyboardType="number-pad"
                                 onChangeText={(_, val) => onDateOfBirthChange(val)}
+                                onFocus={() => onFocusTextInput(3)}
                             />
                             <Spacer mb={2} />
                             <TextInput
@@ -85,7 +135,8 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                 variant="light"
                                 inputKey="zip-code"
                                 keyboardType="number-pad"
-                                onChangeText={(_, val) => val.length <= 5 ? setZipCode(val) : setZipCode(val.slice(0, 5))}
+                                onChangeText={(_, val) => onZipCodeChange(val)}
+                                onFocus={() => onFocusTextInput(4)}
                             />
                             <Spacer mb={4} />
                             <Button block variant="primaryBlack">
@@ -100,13 +151,17 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                                     <TouchableWithoutFeedback>
                                         <Sans style={{ textDecorationLine: "underline" }} size="2" color={color("black100")}>
                                             Login
-                                            </Sans>
+                                        </Sans>
                                     </TouchableWithoutFeedback>
                                 </Text>
                             </Flex>
-                        </ScrollView>
-                    </Box>
-                    <Box p={4} pb={5}>
+                        </Box>
+                        <Box
+                            height={Math.max(0, keyboardHeight - Math.max(0, scrollViewHeight - keyboardBoxMinY) - footerBoxHeight)}
+                            onLayout={e => setKeyboardBoxMinY(e.nativeEvent.layout.y)}
+                        />
+                    </ScrollView>
+                    <Box p={4} pb={5} onLayout={e => setFooterBoxHeight(e.nativeEvent.layout.height)}>
                         <Text style={{ textAlign: "center" }}>
                             <Sans size="1" color="gray">
                                 By creating an account, you agree to our
@@ -131,6 +186,6 @@ export const CreateAccount: React.FC<CreateAccountProps> = (props) => {
                     </Box>
                 </Flex>
             </Flex>
-        </Container>
+        </Container >
     )
 }
