fd0d3b070a254ed2c674dbef577a90b65ae29a98
danielVebman <danveb800@gmail.com>
2020-06-16 16:01:21 -0400

    Add bottom gradient and finish static elements of ChoosePlanPane

--- src/Scenes/CreateAccount/index.tsx
+++ src/Scenes/CreateAccount/index.tsx
@@ -5,5 +5,5 @@
 ///////////////////
 
 // export { ChoosePlanPane as CreateAccount } from "./ChoosePlanPane"
-// export { SetMeasurementsPane as CreateAccount } from "./SetMeasurementsPane"
-export { CreateAccountPane as CreateAccount } from "./CreateAccountPane"
+export { SetMeasurementsPane as CreateAccount } from "./SetMeasurementsPane"
+// export { CreateAccountPane as CreateAccount } from "./CreateAccountPane"
