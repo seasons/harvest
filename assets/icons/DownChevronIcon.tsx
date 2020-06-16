43eed508afb1f9ebdba3606970921dd6ac683502
danielVebman <danveb800@gmail.com>
2020-06-16 19:00:21 -0400

    Add scale to down chevron icon

--- assets/icons/DownChevronIcon.tsx
+++ assets/icons/DownChevronIcon.tsx
@@ -4,14 +4,16 @@ import { View } from "react-native"
 
 interface Props extends SvgProps {
   rotate?: boolean
+  scale?: number
 }
 
 export const DownChevronIcon = (props: Props) => {
   const rotationStyle = props.rotate ? { transform: [{ rotate: "180deg" }] } : {}
+  const scale = props.scale || 1
   const color = props.color ? props.color : "#FFF"
   return (
     <View {...props}>
-      <Svg width={9} height={6} style={rotationStyle}>
+      <Svg width={9 * scale} height={6 * scale} viewBox="0 0 9 6" style={rotationStyle}>
         <Path
           d="M1 5l3.5-4L8 5"
           stroke={color}
