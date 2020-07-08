288cc381c6c2957fcfc2981141b4775ac0ec2645
Luc Succes <l2succes@gmail.com>
2020-07-08 16:31:22 -0400

    Add user identify for analytics

--- src/Navigation/AuthProvider.tsx
+++ src/Navigation/AuthProvider.tsx
@@ -1,13 +1,16 @@
-import { getUserSession } from "App/utils/auth"
-import React, { useEffect } from "react"
-import AuthContext from "./AuthContext"
-import AsyncStorage from "@react-native-community/async-storage"
-import { createStackNavigator } from "@react-navigation/stack"
-import { ModalAndMainScreens } from "./Stacks"
-import { NotificationsProvider } from "App/Notifications"
 import { PopUp } from "App/Navigation/PopUp"
 import { PopUpProvider } from "App/Navigation/PopUp/PopUpProvider"
+import { NotificationsProvider } from "App/Notifications"
+import { getUserSession } from "App/utils/auth"
+import React, { useEffect } from "react"
 import RNPusherPushNotifications from "react-native-pusher-push-notifications"
+
+import AsyncStorage from "@react-native-community/async-storage"
+import { createStackNavigator } from "@react-navigation/stack"
+import analytics from "@segment/analytics-react-native"
+
+import AuthContext from "./AuthContext"
+import { ModalAndMainScreens } from "./Stacks"
 
 // For docs on auth see: https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html
 
@@ -50,6 +53,8 @@ export const AuthProvider = ({ currentScreen, apolloClient }) => {
       try {
         const userSession = await getUserSession()
         if (userSession && userSession.token) {
+          const user = userSession?.user
+          analytics.identify(user?.email, user)
           dispatch({ type: "RESTORE_TOKEN", token: userSession.token })
         }
       } catch (e) {
@@ -63,6 +68,10 @@ export const AuthProvider = ({ currentScreen, apolloClient }) => {
   const authContext = {
     signIn: async (session) => {
       dispatch({ type: "SIGN_IN", token: session.token })
+      const user = session?.user
+      if (user) {
+        analytics.identify(user.email, user)
+      }
       apolloClient.resetStore()
     },
     signOut: async () => {
