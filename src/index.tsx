39a547fc8de16153e29c396ae0756958dd039dc8
Luc Succes <l2succes@gmail.com>
2020-09-04 15:23:38 +0200

    Finish setting up storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
-const STORYBOOK_START = true
-export default STORYBOOK_START ? require("./storybook").default : require("./App").default
+const STORYBOOK_START = false
+export default STORYBOOK_START ? require("./storybook").default : require("./App").App
