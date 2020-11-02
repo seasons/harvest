d205ccf21f45773b7f2d41c258182c8f9d775dba
Kieran Gillen <kgillen@gmail.com>
2020-11-02 16:21:48 -0500

    Removes unused EditShippingAddress file

--- src/Scenes/Account/EditShippingAddress/index.tsx
+++
@@ -1,1 +0,0 @@
-export { EditShippingAddress } from "./EditShippingAddress"
--- src/Scenes/Account/index.tsx
+++ src/Scenes/Account/index.tsx
@@ -1,5 +1,4 @@
 export { Account } from "./Account"
 export { EditMeasurements, InitialMeasurements } from "./EditMeasurements"
-export { EditShippingAddress } from "./EditShippingAddress"
 export { EditStylePreferences } from "./EditStylePreferences"
 export { EditPaymentAndShipping, PaymentAndShipping } from "./PaymentAndShipping"
