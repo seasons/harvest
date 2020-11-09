cea528a71e7a31398a350d65390afadfb94c7424
Daniel Ramirez <javamonn@gmail.com>
2020-11-09 17:27:25 -0500

    fix: prompt contacts permission on bottom sheet expand, don't reprompt when permission is accepted

--- src/Scenes/Account/InviteFriends/InviteFriends.tsx
+++ src/Scenes/Account/InviteFriends/InviteFriends.tsx
@@ -6,6 +6,7 @@ import { screenTrack } from "App/utils/track"
 import React, { useState } from "react"
 import { Dimensions, Share, TouchableOpacity } from "react-native"
 import { useSafeArea } from "react-native-safe-area-context"
+import Contacts from "react-native-contacts"
 
 const windowDimensions = Dimensions.get("window")
 const windowWidth = windowDimensions.width
@@ -19,15 +20,26 @@ export const InviteFriends = screenTrack()(({ route, navigation }) => {
   const onShare = async () => {
     try {
       await Share.share({
-        message: "Here’s my referral link for Seasons. Get $50 off your first month when you sign-up:",
+        message: `Here’s my referral link for Seasons. Get 50% off your first month when you sign-up: ${referralLink}`,
       })
     } catch (error) {
       alert(error.message)
     }
   }
 
-  const onInviteFromContacts = async () => {
-    setOpenPopUp(true)
+  const onPressInviteFromContacts = async () => {
+    const contactPermission = await Contacts.checkPermission()
+    if (contactPermission === "authorized") {
+      navigation.navigate("Modal", { screen: "InviteFromContactsModal", params: { referralLink } })
+    } else {
+      setOpenPopUp(true)
+    }
+  }
+
+  const onPressViewContacts = async () => {
+    setOpenPopUp(false)
+    await Contacts.requestPermission()
+    navigation.navigate("Modal", { screen: "InviteFromContactsModal", params: { referralLink } })
   }
 
   const InviteContactsPopUp = () => {
@@ -41,14 +53,7 @@ export const InviteFriends = screenTrack()(({ route, navigation }) => {
             Choose which friends to invite to Seasons by allowing us to view your contacts.
           </Sans>
           <Spacer mb={4} />
-          <Button
-            block
-            variant="primaryBlack"
-            onPress={() => {
-              setOpenPopUp(false)
-              navigation.navigate("Modal", { screen: "InviteFromContactsModal", params: { referralLink } })
-            }}
-          >
+          <Button block variant="primaryBlack" onPress={onPressViewContacts}>
             View Contacts
           </Button>
           <Spacer mb={1} />
@@ -95,7 +100,7 @@ export const InviteFriends = screenTrack()(({ route, navigation }) => {
         </Box>
         <Separator />
         <Box px={2} pb={insets.bottom} style={{ position: "absolute", bottom: 0 }}>
-          <Button variant="primaryBlack" onPress={onInviteFromContacts} block>
+          <Button variant="primaryBlack" onPress={onPressInviteFromContacts} block>
             Invite from contacts
           </Button>
           <Spacer mb={1} />
