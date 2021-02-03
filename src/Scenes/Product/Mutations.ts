19123d13a1c0916c1f9fbdd05280e0bd68a5265c
Luc Succes <l2succes@gmail.com>
2021-02-03 09:35:20 -0500

    Update createdDraftedOrder

--- src/Scenes/Product/Mutations.ts
+++ src/Scenes/Product/Mutations.ts
@@ -1,31 +1,24 @@
 import gql from "graphql-tag"
 
-export const PRODUCT_VARIANT_CREATE_ORDER = gql`
-  mutation ProductVariantCreateOrder($input: CreateProductVariantDraftOrderInput!) {
-    createProductVariantOrder(input: $input) {
-      total
-      lineItems {
-        amount
-        description
-      }
-      shippingAddress {
-        address1
-        address2
-        city
-        state
-        zipCode
-      }
-    }
-  }
-`
-
 export const PRODUCT_VARIANT_CREATE_DRAFT_ORDER = gql`
   mutation ProductVariantCreateDraftOrder($input: CreateDraftedOrderInput!) {
     createDraftedOrder(input: $input) {
       id
       orderNumber
+      subTotal
+      total
       type
       status
+      items {
+        id
+        recordID
+        recordType
+        needShipping
+        taxRate
+        taxName
+        taxPercentage
+        taxPrice
+      }
       createdAt
       updatedAt
     }
