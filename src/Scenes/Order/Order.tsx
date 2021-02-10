caeed946a477268c32be78b28e2e9a2e1ee7cb6a
Kieran Gillen <kgillen@gmail.com>
2021-02-10 14:49:10 -0500

    Removes log

--- src/Scenes/Order/Order.tsx
+++ src/Scenes/Order/Order.tsx
@@ -69,8 +69,6 @@ export const Order = screenTrack()(({ route, navigation }) => {
   const customer = data?.me?.customer
   const address = data?.me?.customer?.detail?.shippingAddress
 
-  console.log("order", order)
-
   const [submitOrder] = useMutation(SUBMIT_ORDER, {
     onCompleted: (res) => {
       setIsMutating(false)
