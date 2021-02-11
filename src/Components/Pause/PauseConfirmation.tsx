d6cef6ac516d512291882ee3c87ad744cd1a34b8
Kieran Gillen <kgillen@gmail.com>
2021-02-11 15:12:31 -0500

    Adds images

--- src/Components/Pause/PauseConfirmation.tsx
+++ src/Components/Pause/PauseConfirmation.tsx
@@ -21,11 +21,11 @@ export const PauseConfirmation: React.FC<{ navigation: any; route: any }> = scre
   if (viewState === "WithItems") {
     title = "You've successfully paused your membership with items"
     caption = "When you're ready for new styles, just send back your order and resume your membership."
-    imageURL
+    imageURL = "https://seasons-s3.imgix.net/harvest/PausedWithItems.png?w=576&fit=clip&retina=true&fm=webp&cs=srgb"
   } else {
     title = "You've successfully paused your membership without items"
     caption = `Return your items before ${returnDate} or your membership will auto resume & you'll be billed.`
-    imageURL
+    imageURL = "https://seasons-s3.imgix.net/harvest/PausedNoItems.png?w=576&fit=clip&retina=true&fm=webp&cs=srgb"
   }
 
   return (
