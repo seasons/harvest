4f5633b3faee9bde5ee77592545af824b17c4194
Kieran Gillen <kgillen@gmail.com>
2021-03-04 13:44:37 -0500

    Updates generated files

--- src/generated/BagItemProductVariant.ts
+++ src/generated/BagItemProductVariant.ts
@@ -35,8 +35,8 @@ export interface BagItemProductVariant_product_variants_price {
 export interface BagItemProductVariant_product_variants {
   __typename: "ProductVariant";
   id: string;
-  hasRestockNotification: boolean;
   reservable: number;
+  hasRestockNotification: boolean;
   displayShort: string | null;
   displayLong: string | null;
   price: BagItemProductVariant_product_variants_price;
@@ -59,6 +59,7 @@ export interface BagItemProductVariant_price {
   buyNewPrice: number | null;
   buyNewEnabled: boolean;
   buyNewAvailableForSale: boolean | null;
+  buyUsedAvailableForSale: boolean | null;
   buyUsedPrice: number | null;
   buyUsedEnabled: boolean;
 }
