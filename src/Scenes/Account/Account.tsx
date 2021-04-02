437e95f8ff8ece64b31830847bef3b3591ed200d
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-04-02 13:35:44 -0500

    Want --> Ready

--- src/Scenes/Account/Account.tsx
+++ src/Scenes/Account/Account.tsx
@@ -341,7 +341,7 @@ export const Account = screenTrack()(({ navigation }) => {
             authorizedAt={null}
             authorizationWindowClosesAt={null}
             version={"mobile"}
-            title={"Want to Reactivate?"}
+            title={"Ready to Reactivate?"}
             detail="If you'd like to reactivate your account, please request access and we'll get back to you shortly."
           />
         )
