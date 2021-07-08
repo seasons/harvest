59163244828d279c0eac1f67a282890ca68467a7
Kieran Gillen <kgillen@gmail.com>
2021-07-08 16:08:39 -0400

    Fixes refetch query on return flow

--- src/Scenes/Bag/ReturnYourBag.tsx
+++ src/Scenes/Bag/ReturnYourBag.tsx
@@ -14,6 +14,7 @@ import { Button, Flex, Loader } from "@seasons/eclipse"
 
 import { ReturnYourBagItem } from "./Components/ReturnYourBagItem"
 import { ACTIVE_RESERVATION } from "./CurrentRotation"
+import { GetBag_NoCache_Query } from "./BagQueries"
 
 const RETURN_ITEMS = gql`
   mutation ReturnItems($items: [ID!]!) {
@@ -95,7 +96,7 @@ export const ReturnYourBag = () => {
         <FlatList
           data={activeReservation ? activeReservation.products : []}
           ListHeaderComponent={() => (
-            <Box pt={3} pb={1} px={2}>
+            <Box pb={1} px={2}>
               <Spacer mb={80} />
               <Sans size="7" color="black">
                 Return your items
@@ -140,6 +141,8 @@ export const ReturnYourBag = () => {
                   variables: {
                     items: Object.keys(selectedItems),
                   },
+                  awaitRefetchQueries: true,
+                  refetchQueries: [{ query: GetBag_NoCache_Query }],
                 })
               }}
             >
