877f2d03666bfae3a065f099090c64bb94bb2ead
Luc Succes <l2succes@gmail.com>
2021-08-30 15:34:03 -0400

    Update Reservation screen

--- src/Scenes/Reservation/ReservationLineItems.tsx
+++ src/Scenes/Reservation/ReservationLineItems.tsx
@@ -2,13 +2,19 @@ import { Box, Flex, Sans, Spacer } from "App/Components"
 import { SectionHeader } from "App/Components/SectionHeader"
 import React from "react"
 
+export const formatPrice = (price) =>
+  (price / 100 || 0).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  })
+
 export const ReservationLineItems = ({ lineItems }) => {
   let taxTotal = 0
   let total = 0
 
   return (
     <Box>
-      <SectionHeader title="Order summary" />
+      <SectionHeader title="30-day order summary" />
       <Spacer mb={1} />
       {lineItems?.length > 0 && (
         <Box mb={4}>
@@ -16,18 +22,18 @@ export const ReservationLineItems = ({ lineItems }) => {
             taxTotal = taxTotal + lineItem.taxPrice
             total = lineItem.taxPrice + total + lineItem.price
             return (
-              <Flex flexDirection="row" width="100%" justifyContent="space-between" key={index}>
+              <Flex flexDirection="row" width="100%" justifyContent="space-between" key={index} mb={1}>
                 <Sans size="4" color="black50">
                   {lineItem.name}
                 </Sans>
                 <Sans size="4" color="black50">
-                  {`$${lineItem.price / 100}`}
+                  {`${formatPrice(lineItem.price)}`}
                 </Sans>
               </Flex>
             )
           })}
           {taxTotal > 0 && (
-            <Flex flexDirection="row" width="100%" justifyContent="space-between">
+            <Flex flexDirection="row" width="100%" justifyContent="space-between" mb={1}>
               <Sans size="4" color="black50">
                 Taxes
               </Sans>
@@ -37,12 +43,12 @@ export const ReservationLineItems = ({ lineItems }) => {
             </Flex>
           )}
           {(taxTotal !== 0 || lineItems?.length > 1) && (
-            <Flex flexDirection="row" width="100%" justifyContent="space-between">
-              <Sans size="4" color="black50">
+            <Flex flexDirection="row" width="100%" justifyContent="space-between" mb={1}>
+              <Sans size="4" color="black100">
                 Total
               </Sans>
-              <Sans size="4" color="black50">
-                {`$${total / 100}`}
+              <Sans size="4" color="black100">
+                {formatPrice(total)}
               </Sans>
             </Flex>
           )}
