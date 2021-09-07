45118ef009930408f27039fb87e56f118999f77a
Kieran Gillen <kgillen@gmail.com>
2021-09-07 21:55:26 +0100

    Updates ChoosePlanPane

--- assets/svgs/ListCheck.tsx
+++ assets/svgs/ListCheck.tsx
@@ -2,14 +2,11 @@ import { color } from "App/utils"
 import * as React from "react"
 import Svg, { Path } from "react-native-svg"
 
-
 /* SVGR has dropped some elements not supported by react-native-svg: title */
 
 export const ListCheck = (props) => {
   return (
-    <Svg width={18} height={12} viewBox="0 0 14 10" {...props}
-
-    >
+    <Svg width={14} height={10} viewBox="0 0 14 10" {...props}>
       <Path
         d="M1 5.216L5.062 9 13 1"
         stroke={props.feature ? color("black100") : color("black50")}
