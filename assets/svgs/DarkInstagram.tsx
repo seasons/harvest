f1dc06dc0a0306e54e248aac0c42da59cc18feae
Luc Succes <l2succes@gmail.com>
2021-09-14 19:27:42 -0400

    Update IG icon

--- assets/svgs/DarkInstagram.tsx
+++ assets/svgs/DarkInstagram.tsx
@@ -5,15 +5,11 @@ import Svg, { Circle, G, Path, Rect } from "react-native-svg"
 
 export const DarkInstagram = (props) => {
   return (
-    <Svg {...props} xmlns="http://www.w3.org/2000/svg" width={26} height={36} viewBox="0 0 26 36">
+    <Svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" {...props}>
       <G fill="none" fillRule="evenodd">
-        <Path fill="#FFF" d="M0 0h26v36H0z" />
-        <Path fill="#FFF" d="M1 5h24v26H1z" />
-        <G transform="translate(1 6)">
-          <Rect width={24} height={24} fill="#D8D8D8" rx={6} />
-          <Circle cx={12} cy={12} r={5} stroke="#FFF" strokeWidth={2} />
-          <Circle cx={18.5} cy={5.5} r={1.5} fill="#FFF" />
-        </G>
+        <Rect stroke="#000" strokeWidth={1.5} x={0.75} y={0.75} width={22.5} height={22.5} rx={6} />
+        <Circle stroke="#000" strokeWidth={1.5} cx={12} cy={12} r={4.25} />
+        <Circle fill="#000" cx={18.5} cy={5.5} r={1.5} />
       </G>
     </Svg>
   )
