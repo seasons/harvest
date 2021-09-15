84908ddccd30df24cece17f6140e9efddea2b892
Luc Succes <l2succes@gmail.com>
2021-09-15 14:30:02 -0400

    Move bottom cards to separate component

--- src/Scenes/Bag/Components/BagTab.tsx
+++ src/Scenes/Bag/Components/BagTab.tsx
@@ -1,72 +1,35 @@
-import { Flex, Sans, Separator } from "App/Components"
-import { PauseStatus, RESUME_MEMBERSHIP } from "App/Components/Pause/PauseButtons"
-import { GetBag_NoCache_Query as GetBag_NoCache_Query_Type } from "App/generated/GetBag_NoCache_Query"
+import { Flex, Separator } from "App/Components"
+import {
+  GetBag_NoCache_Query as GetBag_NoCache_Query_Type
+} from "App/generated/GetBag_NoCache_Query"
 import { MAXIMUM_ITEM_COUNT } from "App/helpers/constants"
-import { Schema as NavigationSchema } from "App/Navigation"
 import { useBottomSheetContext } from "App/Navigation/BottomSheetContext"
-import { usePopUpContext } from "App/Navigation/ErrorPopUp/PopUpContext"
-import { color, space } from "App/utils"
-import { DarkInstagram, Stylist } from "Assets/svgs"
+import { color } from "App/utils"
 import { assign, fill } from "lodash"
 import { DateTime } from "luxon"
-import React, { useState } from "react"
-import { Dimensions, Linking, ScrollView } from "react-native"
+import React from "react"
 
-import { useMutation } from "@apollo/client"
 import { useNavigation } from "@react-navigation/native"
 import { Box, ProductBuyAlertTab, ProductBuyAlertTabType, Spacer } from "@seasons/eclipse"
-import * as Sentry from "@sentry/react-native"
 
-import { GetBag_NoCache_Query } from "../BagQueries"
-import { BagCardButton } from "./BagCardButton"
 import { BagItem } from "./BagItem"
+import { BagTabBottomCards } from "./BagTabBottomCards"
 import { BagTabHeader } from "./BagTabHeader"
 import { BuyBottomSheet, height as bottomSheetHeight } from "./BuyBottomSheet"
 import { EmptyBagItem } from "./EmptyBagItem"
 
-const dimensions = Dimensions.get("window")
-const windowWidth = dimensions.width
-
 export const BagTab: React.FC<{
   bagItems
-  pauseStatus: PauseStatus
   data: GetBag_NoCache_Query_Type
   items
   deleteBagItem
   removeFromBagAndSaveItem
-}> = ({ bagItems, pauseStatus, data, deleteBagItem, removeFromBagAndSaveItem }) => {
-  const [isMutating, setIsMutating] = useState(false)
-  const { showPopUp, hidePopUp } = usePopUpContext()
+}> = ({ bagItems, data, deleteBagItem, removeFromBagAndSaveItem }) => {
   const { bottomSheetSetProps, bottomSheetSnapToIndex } = useBottomSheetContext()
   const navigation = useNavigation()
 
   const me = data?.me
   const activeReservation = me?.activeReservation
-  const hasActiveReservation = !!activeReservation
-
-  const [resumeSubscription] = useMutation(RESUME_MEMBERSHIP, {
-    refetchQueries: [
-      {
-        query: GetBag_NoCache_Query,
-      },
-    ],
-    onCompleted: () => {
-      navigation.navigate("Modal", { screen: NavigationSchema.PageNames.ResumeConfirmation })
-      setIsMutating(false)
-    },
-    onError: (err) => {
-      const popUpData = {
-        title: "Oops!",
-        note: "There was an error resuming your membership, please contact us.",
-        buttonText: "Close",
-        onClose: () => hidePopUp(),
-      }
-      console.log("err", err)
-      Sentry.captureException(err)
-      showPopUp(popUpData)
-      setIsMutating(false)
-    },
-  })
 
   const onShowBuyBottomSheet = (bagItem) => {
     const price = bagItem?.productVariant?.price || {
@@ -108,11 +71,6 @@ export const BagTab: React.FC<{
     bottomSheetSnapToIndex(0)
   }
 
-  const pauseRequest = me?.customer?.membership?.pauseRequests?.[0]
-  const isPaused = pauseStatus === "paused"
-  const pauseType = pauseRequest?.pauseType
-  const withOrWithoutDisplay = pauseType === "WithoutItems" ? "without items" : "with items"
-  const pausedWithoutItems = isPaused && pauseType === "WithoutItems"
   const status = activeReservation?.status
   const updatedMoreThan24HoursAgo =
     activeReservation?.updatedAt && DateTime.fromISO(activeReservation?.updatedAt).diffNow("days")?.values?.days <= -1
@@ -122,47 +80,11 @@ export const BagTab: React.FC<{
   const paddedItems =
     assign(fill(new Array(Math.min(bagItems.length + 1, itemCount)), { variantID: "", productID: "" }), bagItems) || []
 
-  const showShareIGCard = hasActiveReservation && !isPaused
-  const showBottomCards = !isPaused
-
   const showBagItems = bagItems?.length > 0
 
   return (
     <Flex height="100%">
       <BagTabHeader atHome={atHome} me={me} />
-      {isPaused && (
-        <>
-          <Box px={2}>
-            <Separator color={color("black10")} />
-          </Box>
-          <Box px={2} py={2}>
-            <Sans size="4" color="black50">
-              {`Your membership is paused ${withOrWithoutDisplay} until ${DateTime.fromISO(
-                pauseRequest.resumeDate
-              ).toFormat("EEEE LLLL dd")}. To continue reserving earlier, resume it by tapping `}
-              <Sans
-                size="4"
-                style={{ textDecorationLine: "underline" }}
-                onPress={async () => {
-                  if (isMutating) {
-                    return
-                  }
-                  setIsMutating(true)
-                  const subscriptionId = me?.customer?.membership?.subscription?.id
-                  await resumeSubscription({
-                    variables: {
-                      subscriptionID: subscriptionId,
-                    },
-                  })
-                }}
-              >
-                here
-              </Sans>
-              .
-            </Sans>
-          </Box>
-        </>
-      )}
 
       {!showBagItems && (
         <Flex style={{ flex: 1 }} justifyContent="center" alignItems="center">
@@ -172,9 +94,6 @@ export const BagTab: React.FC<{
 
       {showBagItems &&
         paddedItems?.map((bagItem, index) => {
-          if (pausedWithoutItems) {
-            return null
-          }
           const isReserved = !!bagItem?.status && bagItem?.status === "Reserved"
           return bagItem?.productID?.length > 0 ? (
             <Box key={index} pt={isReserved && index !== 0 ? 1 : 2}>
@@ -204,43 +123,10 @@ export const BagTab: React.FC<{
           )
         })}
 
-      {showBottomCards && (
-        <Box mt={3}>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
-            {showShareIGCard && (
-              <Box ml={2} mr={1}>
-                <BagCardButton
-                  Icon={DarkInstagram}
-                  title="Share to IG Stories"
-                  caption="Post your new styles to Instagram"
-                  onPress={() =>
-                    navigation.navigate("Modal", {
-                      screen: "ShareReservationToIGModal",
-                      params: { reservationID: activeReservation?.id },
-                    })
-                  }
-                />
-              </Box>
-            )}
-            {!isPaused && (
-              <Box ml={showShareIGCard ? 0 : 2} mr={1}>
-                <BagCardButton
-                  width={showShareIGCard ? "auto" : windowWidth - space(4)}
-                  Icon={Stylist}
-                  title="Chat with our stylist"
-                  caption="Get a personalized consultation"
-                  onPress={() =>
-                    Linking.openURL(
-                      "mailto:membership@seasons.nyc?subject=Chat%20with%20a%20stylist&body=I%20would%20like%20to%20chat%20with%20a%20seasons%20stylist%20to%20help%20find%20items%20that%20suit%20me.%20Thanks!"
-                    )
-                  }
-                />
-              </Box>
-            )}
-          </ScrollView>
-          {showBagItems && <Spacer mb={90} />}
-        </Box>
-      )}
+      <Box mt={3}>
+        <BagTabBottomCards reservation={activeReservation} />
+        {showBagItems && <Spacer mb={90} />}
+      </Box>
     </Flex>
   )
 }
