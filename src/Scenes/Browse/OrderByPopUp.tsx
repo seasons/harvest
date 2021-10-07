4d2e294ae5d40968f1e80bee4e68ca9ffbec7e4b
Kieran Gillen <kgillen@gmail.com>
2021-10-07 14:57:26 +0200

    Adds order by popup

---
+++ src/Scenes/Browse/OrderByPopUp.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+import { PopUp, Box, Sans, Separator, Spacer, Flex, Radio } from "App/Components"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import { OrderBy } from "./Browse"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+export const OrderByPopUp = ({ show, setOrderBy, orderBy, setShowOrderPopUp }) => {
+  const insets = useSafeAreaInsets()
+
+  const orders = [
+    { display: "New Arrivals", value: OrderBy.publishedAt_DESC },
+    { display: "Price per month: High to low", value: OrderBy.computedRentalPrice_DESC },
+    { display: "Price per month: Low to high", value: OrderBy.computedRentalPrice_ASC },
+  ]
+
+  return (
+    <PopUp show={show} paddingBottom={90 + insets.bottom}>
+      <Box pt={4} pb={2}>
+        <Sans size="4" textAlign="center">
+          Sort by
+        </Sans>
+      </Box>
+      <Separator />
+      {orders.map((order, index) => {
+        const selected = orderBy === order.value
+        return (
+          <Box key={index} px={2}>
+            <TouchableOpacity
+              onPress={() => {
+                setOrderBy(order.value)
+                setShowOrderPopUp(false)
+              }}
+            >
+              <Flex py={2} flexDirection="row" justifyContent="space-between" flexWrap="nowrap">
+                <Sans size="4">{order.display}</Sans>
+                <Radio selected={selected} />
+              </Flex>
+            </TouchableOpacity>
+            <Separator />
+          </Box>
+        )
+      })}
+      <Spacer mb={3} />
+      <TouchableOpacity onPress={() => setShowOrderPopUp(false)}>
+        <Sans underline size="4" textAlign="center">
+          Cancel
+        </Sans>
+      </TouchableOpacity>
+    </PopUp>
+  )
+}
