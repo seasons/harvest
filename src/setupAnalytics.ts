cef69012dc1e67e8a58881131e8b80cc3007686a
Kieran Gillen <kgillen@gmail.com>
2021-10-10 00:05:46 +0200

    Removes mixpanel

--- src/setupAnalytics.ts
+++ src/setupAnalytics.ts
@@ -1,11 +1,7 @@
 import { config, Env } from "App/utils/config"
 import analytics from "@segment/analytics-react-native"
-// import GoogleAnalytics from "@segment/analytics-react-native-google-analytics"
-// import Mixpanel from "@segment/analytics-react-native-mixpanel"
 
 export default analytics.setup(config.get(Env.SEGMENT_KEY), {
-  // using: [Mixpanel, GoogleAnalytics],
-  // recordScreenViews: true,
   trackAppLifecycleEvents: true,
   trackAttributionData: true,
 
